8012e5819eb27247860fc011a0a4d33daa6102ef
Sofia Protsiv <prochivsofia@gmail.com>
2022-02-10 17:13:35 +0200

    add files

--- src/scripts/formula.js
+++ src/scripts/formula.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-const create = document.querySelector('[data-create]');
-const destroy = document.querySelector('[data-destroy]');
-const boxes = document.getElementById('boxes');
-const inputEl = document.querySelector('input');
+const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+const boxes = document.getElementById("boxes");
+const inputEl = document.querySelector("input");
 
-create.addEventListener('click', getWidth);
-destroy.addEventListener('click', destroyBoxes);
+create.addEventListener("click", getWidth);
+destroy.addEventListener("click", destroyBoxes);
 
 function getWidth() {
-  const width = document.querySelector('#controls input').value;
+  const width = document.querySelector("#controls input").value;
   if (width) {
     createBoxes(width);
   }
@@ -17,23 +17,23 @@ function getWidth() {
 
 export default function createBoxes(width) {
   const fragment = document.createDocumentFragment();
-  boxes.innerHTML = '';
+  boxes.innerHTML = "";
 
   const size = width * 37.7;
 
   //////////BOX///////////
-  const div = document.createElement('div');
-  div.classList.add('square');
+  const div = document.createElement("div");
+  div.classList.add("square");
   div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
 
   /////////FORMULA 1 ////////////////
-  const p1Ele = document.createElement('p');
-  p1Ele.classList.add('formulaSquare');
+  const p1Ele = document.createElement("p");
+  p1Ele.classList.add("formulaSquare");
   p1Ele.textContent = `S = H² = ${width}sm * ${width}sm = ${width * width}sm²`;
 
   /////////FORMULA 2 ////////////////
-  const p2Ele = document.createElement('p');
-  p2Ele.classList.add('formulaSquare');
+  const p2Ele = document.createElement("p");
+  p2Ele.classList.add("formulaSquare");
 
   p2Ele.textContent = `V = H³ = (${width}sm * ${width}sm * ${width}sm) = ${
     width * width * width
@@ -44,12 +44,12 @@ export default function createBoxes(width) {
   fragment.appendChild(p1Ele);
   fragment.appendChild(p2Ele);
 
-  inputEl.value = '';
+  inputEl.value = "";
   boxes.appendChild(fragment);
 }
 
 function destroyBoxes() {
-  boxes.innerHTML = '';
+  boxes.innerHTML = "";
 }
 
 function getRandomHexColor() {
